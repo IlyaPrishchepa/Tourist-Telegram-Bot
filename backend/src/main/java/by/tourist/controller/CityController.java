ef0d1f3770de6019eadcfa3c06d5a15a35dd983a
Ilya Prishchepa <ilya.prishchepa@gmail.com>
2021-05-05 00:15:36 +0300

    Create controller class

---
+++ backend/src/main/java/by/tourist/controller/CityController.java
@@ -0,0 +1,42 @@
+package by.tourist.controller;
+
+import by.tourist.entity.City;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import by.tourist.services.implementation.CityServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/city")
+public class CityController {
+
+    @Autowired
+    private CityServiceImpl cityService;
+
+    @PostMapping
+    public City create( @RequestBody City city) {
+        return cityService.create(city);
+    }
+
+    @GetMapping("/find-all")
+    public List<City> findAll(@RequestParam int page,@RequestParam int size) {
+        return cityService.findAll(page, size);
+    }
+
+    @GetMapping("/find-by-name")
+    public List<City> findByName(@RequestParam String name,@RequestParam int page,@RequestParam int size) {
+        return cityService.findByName(name, page, size);
+    }
+
+    @GetMapping("/size")
+    public int getSize() {
+        return cityService.getSize();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable int id) {
+        cityService.deleteById(id);
+
+    }
+}
