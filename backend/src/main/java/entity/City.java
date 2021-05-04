db7282a4578c9006b4e803c0518edad58561ee6f
Ilya Prishchepa <ilya.prishchepa@gmail.com>
2021-05-04 22:24:37 +0300

    Create entity class

---
+++ backend/src/main/java/entity/City.java
@@ -0,0 +1,26 @@
+package entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Basic
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Basic
+    @Column(name = "description")
+    private String description;
+}
